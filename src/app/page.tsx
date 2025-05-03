a06d38ee5fe762ebabcfb5bdf1cc498e8a9d098e
Sudhar08 <99385366+sudhar08@users.noreply.github.com>
2025-05-03 13:24:54 +0530

    update page,tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Navbar from '../components/Navbar';
+import Navbar from '../components/NavBar';
 import SearchBar from '../components/search';
 import JobListings from '../components/joblisting';
 
