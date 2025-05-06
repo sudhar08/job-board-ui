42b5ba056e0f31004fb38c6c2bf64f50106f8be7
Sudhar08 <99385366+sudhar08@users.noreply.github.com>
2025-05-06 18:09:31 +0530

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from '../components/NavBar';
-import SearchBar from '../components/search';
-import JobListings from '../components/joblisting';
+
 import JobPage from "../components/jobpage";
 
 export default function Home() {
